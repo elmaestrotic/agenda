1b878c9b819991ca73b8dd8ab8dc85bba30dde41
elmaestrotic <45459513+elmaestrotic@users.noreply.github.com>
2023-07-31 14:06:45 -0500

    Front end de la agenda de usuarios

--- src/main/java/com/narvasoft/agenda/controller/UsuariosController.java
+++ src/main/java/com/narvasoft/agenda/controller/UsuariosController.java
@@ -5,9 +5,13 @@ import com.narvasoft.agenda.repo.UsuariosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 public class UsuariosController {
@@ -15,18 +19,27 @@ public class UsuariosController {
     private UsuariosRepository usuariosRepository;
 
     @GetMapping("")
-    public String index() {
+    public String index(Model model) {
+        List<Usuarios> usuarios = usuariosRepository.findAll();
+        model.addAttribute("usuarios", usuarios);
         return "index";
     }
 
     @GetMapping("/nuevo")
     public String nuevo(Model model) {//<--se crea un objeto modelo de tipo usuario
-        model.addAttribute("Usuario", new Usuarios());//<--se agrega el objeto usuario al modelo");
+        model.addAttribute("usuario", new Usuarios());//<--se agrega el objeto usuario al modelo");
         return "nuevo";
     }
 
     @PostMapping("/nuevo")
-    public String crear(Usuarios usuario, RedirectAttributes attr) {
+    public String crear(@Validated Usuarios usuario,
+                        BindingResult bindingResult,
+                        Model model,
+                        RedirectAttributes attr) {
+       if (bindingResult.hasErrors()) {
+           model.addAttribute("usuario", usuario);
+            return "nuevo";
+        }
         usuariosRepository.save(usuario);
         attr.addFlashAttribute("msgExito", "Usuario creado exitosamente");
         return "redirect:/";
